7d6c2a63de716f55e8621a4b2fbaab48497c1cec
unknown <jk449738@gmail.com>
2022-05-04 21:51:44 +0530

    2 api done

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -12,20 +12,22 @@ const createColleges = async function (req, res) {
         if (Object.keys(data).length == 0) {
             return res.status(400).send({ status: false, msg: 'Invalid Request !! Please Enter college Detail ' })
         }
+
+        const { name, fullName, logoLink } = data
         // check that user is provide only string
-        if ((data.name == 0) || !dv.test(data.name)) {
+        if ((name == 0) || !dv.test(name)) {
             return res.status(400).send({ status: false, msg: "please provide valid name of college" })
         }
-        if ((data.fullName == 0) || !dv.test(data.fullName)) {
-            return res.status(400).send({ status: false, msg: "please provide valid Fullname of college" })
-        }
-        if (data.logoLink == 0) {
-            return res.status(400).send({ status: false, msg: "please provide logoLink" })
-        }
         //  check if the intern is already registerd or not
-        let existingCollege = collegeModel.find({ name: data.name })
+        let existingCollege = await collegeModel.findOne({name})
         if (existingCollege) {
             return res.status(400).send({ status: false, msg: "college is already exist" })
+        }
+        if ((fullName == 0) || !dv.test(fullName)) {
+            return res.status(400).send({ status: false, msg: "please provide valid Fullname of college" })
+        }
+        if (logoLink == 0) {
+            return res.status(400).send({ status: false, msg: "please provide logoLink" })
         }
         let college = await collegeModel.create(data)
         return res.status(201).send({ msg: "Data created successfully", data: college })
@@ -42,37 +44,39 @@ const createInterns = async function (req, res) {
         if (Object.keys(data).length == 0) {
             return res.status(400).send({ status: false, msg: 'Invalid Request !! Please Enter intern Detail ' })
         }
+        
+        const { name, collegeName, email,mobile, collegeId } = data
         // check that user is provide only string
-        if ((data.name == 0) || !dv.test(data.name)) {
+        if ((name == 0) || !dv.test(name)) {
             return res.status(400).send({ status: false, msg: "please provide name of intern" })
         }
-        if ((data.collegeName == 0) || !dv.test(data.collegeName)) {
+        if ((collegeName == 0) || !dv.test(collegeName)) {
             return res.status(400).send({ status: false, msg: "please provide name of college" })
         }
         // email validation using regex
-        if ((data.email == 0) || !evalid.test(data.email)) {
+        if ((email == 0) || !evalid.test(email)) {
             return res.status(400).send({ status: false, msg: "please provide a valid email" })
         }
         // checking email in database
-        existingemail = await internModel.findOne({ email: data.email })
+        existingemail = await internModel.findOne({ email:email })
         if (existingemail) {
             return res.status(400).send({ msg: "email is already exist" })
         }
         // mobile validation using regex
-        if ((data.mobile == 0) || !mob.test(data.mobile)) {
+        if ((mobile == 0) || !mob.test(mobile)) {
             return res.status(400).send({ status: false, msg: " moblie number should be of 10 digit" })
         }
         // checking mobile in database
-        existingMobile = await internModel.findOne({ mobile: data.mobile })
+        existingMobile = await internModel.findOne({ mobile: mobile })
         if (existingMobile) {
             return res.status(400).send({ msg: "Mobile no. is already exist" })
         }
         // check the collegeId is valid type of Id or blank
-        if ((data.collegeId == 0) || !ObjectId.isValid(data.collegeId)) {
+        if ((collegeId == 0) || !ObjectId.isValid(collegeId)) {
             return res.status(400).send({ status: false, msg: "please provide a valid  collegeId" })
         }
         // check te collegID is present or not
-        checkCollegeId = await collegeModel.findById({ _id: data.collegeId })
+        checkCollegeId = await collegeModel.findById({ _id: collegeId })
         if (!checkCollegeId) {
             return res.status(400).send({ status: false, msg: "college id is not found" })
         }
